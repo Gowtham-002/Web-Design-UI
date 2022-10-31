b97884ba01484136a3545f8138a651c50c5c4db7
Gowtham-002 <raghuraman.g@northeastern.edu>
2022-10-31 02:54:47 -0400

    Personal Portfolio

---
+++ react-portfolio/src/components/Home/Logo/index.js
@@ -0,0 +1,9 @@
+import LogoS from '../../../assets/images/Gowtham_logo.png'
+import './index.scss'
+
+const Logo = () => {
+  return
+  ;<img src={LogoS} alt="About Image" />
+}
+
+export default Logo
